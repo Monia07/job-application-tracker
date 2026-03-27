a934320fabd69e371c3926ef6af45f9137b6056a
monia07 <monia_7@hotmail.com>
2026-03-27 03:23:38 +0100

    Add __init__ to JobApplication class

---
+++ application.py
@@ -0,0 +1,6 @@
+class JobApplication:
+    def __init__(self, id, company_name, job_title):
+        self.id = id
+        self.company_name = company_name
+        self.job_title = job_title
+        
