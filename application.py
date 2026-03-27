7198844785fd5bf83ea867de6f2aa5a514dba5a7
monia07 <monia_7@hotmail.com>
2026-03-27 03:49:52 +0100

    Add full data model fields to JobApplication

--- application.py
+++ application.py
@@ -1,6 +1,27 @@
 class JobApplication:
-    def __init__(self, id, company_name, job_title):
+    def __init__(
+        self,
+        id,
+        company_name,
+        job_title,
+        status,
+        date_applied,
+        deadline=None,
+        contact_name=None,
+        contact_email=None,
+        follow_up_date=None,
+        job_link=None,
+        notes=None
+    ):
         self.id = id
         self.company_name = company_name
         self.job_title = job_title
+        self.status = status
+        self.date_applied = date_applied
+        self.deadline = deadline
+        self.contact_name = contact_name
+        self.contact_email = contact_email
+        self.follow_up_date = follow_up_date
+        self.job_link = job_link
+        self.notes = notes
         
