cd7393c10defe31c3df5f3cb83dd0f2bd628bd32
monia07 <monia_7@hotmail.com>
2026-04-10 04:01:41 +0200

    Display JobApplication data from database in list view

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import JobApplication
+
 
 def application_list(request):
-    return render(request, "tracker/application_list.html")
-
+    applications = JobApplication.objects.all()
+    return render(
+        request,
+        "tracker/application_list.html",
+        {"applications": applications},
+    )
