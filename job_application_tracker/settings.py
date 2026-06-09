9393510b38f8cf53760a10f6884065b66a449b7d
monia07 <monia_7@hotmail.com>
2026-06-09 14:12:38 +0200

    Install and configure django allauth

--- job_application_tracker/settings.py
+++ job_application_tracker/settings.py
@@ -24,6 +24,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'allauth',
+    'allauth.account',
+
     'tracker',
 ]
 
@@ -36,6 +40,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
